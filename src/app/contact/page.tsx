9231ad2bb62a9a702ef62057adfd645c289f5835
Addagarla <muraliaddagarla01@gmail.com>
2024-02-27 15:23:54 +0530

    first-commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,26 @@
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Contact from "@/components/Contact";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Page",
+  // description: "This is Contact Page for Startup Nextjs Template",
+  // other metadata
+};
+
+const ContactPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="Contact Page"
+        description=" " 
+              />
+
+
+      <Contact />
+    </>
+  );
+};
+
+export default ContactPage;
---
+++ src/components/Features/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Main Features"
+            paragraph="Dhara Realty is changing to focus on selling homes in gated communities in Hyderabad's western area. They'll work closely with landlords, sell directly, and team up with others who can refer buyers and earn commissions. This shift aims to make the sales process simpler and more targeted.."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
